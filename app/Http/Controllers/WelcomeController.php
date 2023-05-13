1c18d2eb9e821cc060513089ca66d05e98fddb73
Roger Moore <john_1726@yahoo.com>
2023-05-13 15:28:47 -0600

    Added code to verify data added to first_app table using DataGrip

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -10,8 +10,8 @@ class WelcomeController extends Controller
 {
     public function index() {
         //1) Using raw SQL queries
-        //$users = DB::select("select * from users");
-        //dd($users);
+        $users = DB::select("select * from users");
+        dd($users);
         //2) Query Builder
         //$users = DB::table('users')->select(['name', 'email'])->whereNotNull('email')->orderBy('name')->get();
         //dd($users);
@@ -22,9 +22,9 @@ class WelcomeController extends Controller
         //foreach($students as $student) {
         //  echo $student->name. "<br>";
         //}  
-        $student = new Student(); 
-        $student->name = "New";
-        $student->email = "email@email.com";
-        $student->save();
+        //$student = new Student(); 
+        //$student->name = "New";
+        //$student->email = "email@email.com";
+        //$student->save();
     }
 }
